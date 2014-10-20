367b04405c25774ea2a0208cc20e94e6c81d72ca
kuantero <kuantero@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-20 07:28:46 +0000

    feedback file added
    git-svn-id: https://plugins.svn.wordpress.org/59sec-lite-contact-form-7-push-notifications-on-ios-and-android/trunk@1010418 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/59sec_feedback.php
@@ -0,0 +1,32 @@
+<p>UNANSWERED LEADS: <?php echo $leadsModel->getTotalUnansweredLeads()?></p>
+<ul class="subsubsub">
+	<li><?php echo $leadsLink?></li>
+	<li><?php echo $crmLink?></li>
+</ul>
+<br class="clear-subsubsub"/>
+<ul class="subsubsub">
+	<li><?php echo $statisticsLink?></li>
+	<li><?php echo $sourcesLink?></li>
+	<li><?php echo $usersLink?></li>
+	<li><?php echo $notificationsLink?></li>
+	<li><?php echo $otherOptionsLink?></li>
+	<li><?php echo $helpLink?></li>
+</ul>
+<h2>Please Feedback!</h2>
+ <p>Hello dear ...<br />
+- entrepreneurs,<br />
+- sales managers,<br />
+- business owners,<br />
+- CEOs<br />
+- and people interested in getting more results! :)</p>
+<p>At 59sec we really need your feedback. Your suggestions & comments..</p>
+<p>We are 100% committed to help your business grow, so you’ll be happy and recommend us further. :)
+59sec is a CRITICAL business service, so our dialog is more than needed.</p>
+<p>So, please, talk to us:<br />
+<a href="mailto:go@59sec.com">go@59sec.com</a></p>
+<p>We’ll be happy to hear from you.</p>
+<p>
+Your truly,<br />
+Constantin Ferseta<br />
+Founder 59sec<br />
+</p>
